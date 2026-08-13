458c925df1b01d7b6870c64e53c313b3cc31700a
Syed Mesum Ali Shah <s.mesumali99@gmail.com>
2026-08-14 00:08:30 +0500

    add cache

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 const environment = process.env.APP_ENV || "development";
 const version = process.env.APP_VERSION || "1.0.0";
+const apiKey = process.env.API_KEY || 2565656565;
+
+console.log("Environment:", environment);
+console.log("Version:", version);
+console.log("API Key:", apiKey);
 
 module.exports = {
     environment,
-    version
+    version,
+    apiKey
 };
